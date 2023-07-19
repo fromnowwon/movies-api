05f79811dfadca9d871de964948665b1626dabbb
cw <fromnowwon@gmail.com>
2023-07-19 20:53:36 +0900

    first commit

---
+++ src/main/java/dev/fromnowwon/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package dev.fromnowwon.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // 해당 클래스가 애플리케이션의 주 진입점임을 나타냄
+public class MoviesApplication { // 실제 애플리케이션의 메인 클래스
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
